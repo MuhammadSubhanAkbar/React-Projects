21e0935b7a4e9f27ce2c2f16987d2e1c94645fc3
Subhan Akbar <subhanakbar346@gmail.com>
2025-10-07 19:45:20 +0500

    Progress

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -2,16 +2,18 @@ import Navigation from "./nav/Nav.jsx";
 import Products from "./products/Products.jsx";
 import Recommendations from "./recommendations/Recommendation.jsx";
 import Categories from "./sidebar/catogory/Catogory.jsx";
+import Sidebar from "./sidebar/Sidebar.jsx";
 
 
 function App() {
 
   return (
     <>
+        <Sidebar />
         <Recommendations />
         <Navigation />
+
         <Products />
-        <Categories />
     </>
   )
 }
